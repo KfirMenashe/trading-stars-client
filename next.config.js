2ef60222024695c7be99cf541f41cc77564a5278
kfir Menashe <kfir.menashe@gmail.com>
2022-02-06 17:08:17 +0200

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env:{
+    appName:'TRADINGSTARS'
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '../components/layout/Layout'
+import Emitter, { EmitterEvents } from '../services/emitter';
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const emitter = Emitter.get();
+  emitter.on(EmitterEvents.LoginClicked, () => {
+    router.push('/login');
+  });
+  return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+  );
 }
 
 export default MyApp
